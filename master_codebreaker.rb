1379be9ed19f7d1ee8850e2a17e62d7013e48a49
Mark D. King <kingmarkd@gmail.com>
2018-02-03 20:26:24 -0500

    update description

---
+++ master_codebreaker.rb
@@ -0,0 +1,13 @@
+class GameLogic {
+
+}
+
+class Board {
+
+}
+
+player = Struct.new(:name, :wins, :losses, :best)
+
+code = Struct.new(:code1, :code2, :code3, :code4)
+
+color = ["green", "blue", "red", "purple", "yellow", "orange"]
